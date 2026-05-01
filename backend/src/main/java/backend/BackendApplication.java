9066b042efe21b194cb98ddacfd6574b4a917aee
Clint Villaflores <118436424+Ryatsu@users.noreply.github.com>
2026-05-01 14:58:59 +0800

    URI safe

--- backend/src/main/java/backend/BackendApplication.java
+++ backend/src/main/java/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
+
 		SpringApplication.run(BackendApplication.class, args);
 		System.out.println("Server Running...");
 	}
